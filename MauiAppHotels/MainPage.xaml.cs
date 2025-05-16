f61aaa4e4dd4533a3d700052e2cbaf01cd1a6403
KaiqueSonDz <kaique2369@hotmail.com>
2025-05-16 14:54:34 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotels/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiAppHotels
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+
+}
---
+++ MauiAppHotels/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppHotels
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
